79378237e481154d233511df65bebd8a729394b8
SyahrulR-project <65535593+SyahrulR-project@users.noreply.github.com>
2020-05-22 16:18:42 +0700

    Update create.php

--- docs/create.php
+++ docs/create.php
@@ -1,1 +1,106 @@
 
+<html>
+<head>
+<title>Create Script Deface</title>
+<link rel="icon" href="https://j.top4top.io/p_1603k8mm60.jpg">
+<meta name="description" content="Tools membuat Script Deface | Syahrul R.">
+<link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet" type="text/css">
+<style>
+body {
+  background-image:url(https://e.top4top.io/p_1601lthln0.jpg);
+  background-size:cover;
+}
+h1 {
+  color:yellow;
+  font-size:90px;
+  font-family:'Kalam', sans-serif;
+  text-shadow:4px 4px 4px green;
+  margin-bottom:5px;
+}
+h2 {
+  color:red;
+  font-family:'Kalam' , sans-serif;
+  font-size:30px;
+  text-shadow:4px 4px 4px yellow;
+}
+.title {
+  color:red;
+  font-size:50px;
+  font-family:'Kalam' , sans-serif;
+  text-shadow:4px 4px 4px white;
+  margin-bottom:5px;
+}
+.boxtitle {
+  color:red;
+  font-size:40px;
+  background-color:transparent;
+}
+.gambar {
+  color:red;
+  font-size:50px;
+  font-family:'Kalam' , sans-serif;
+  text-shadow:4px 4px 4px white;
+  margin-bottom:5px;
+}
+.boxgambar {
+  color:red;
+  font-size:50px;
+  background-color:transparent;
+}
+.nick {
+  color:red;
+  font-size:50px;
+  font-family:'Kalam' , sans-serif;
+  text-shadow:4px 4px 4px white;
+  margin-bottom:5px;
+}
+.boxnick {
+  color:red;
+  font-size:50px;
+  background-color:transparent;
+}
+.kata {
+  color:red;
+  font-size:50px;
+  font-family:'Kalam' , sans-serif;
+  text-shadow:4px 4px 4px white;
+  margin-bottom:5px;
+  }
+.boxkata {
+  color:red;
+  width:600px;
+  font-size:40px;
+  background-color:transparent;
+  margin-bottom:30px;
+}
+.tombol {
+  color:red;
+  background-color:transparent;
+  font-size:50px;
+  
+}
+</style>
+</head>
+<body>
+<center>
+<h1>Create Script Deface</h1>
+<form method="post" action="script.php">
+<p class="title">Judul / Title : </p><input type="text" name="title" class="boxtitle" placeholder="Hacked by Syahrul R."></input>
+<br>
+<p class="gambar">link gambar / logo</p>
+<input type="text" name="link" class="boxgambar" placeholder="https://link-gambar.com/gambar.jpg"></input>
+<br>
+<p class="gambar">Link Gambar Background</p>
+<input type="text" name="bg" class="boxgambar" placeholder="https://gambar.com/gambar.jpg"></input>
+<br>
+<p class="nick">Nick Hacker :</p><input type="text" name="nick" class="boxnick" placeholder="Syahrul R."></input>
+<br>
+<p class="kata">Kata kata :</p> <textarea name="kata" class="boxkata" placeholder="We From Laughsec Team , Your Security very ezz"></textarea>
+<br>
+<input type="Submit" name="submit" class="tombol" value="create"></input>
+</form>
+<h2>Coded by Syahrul R. 2k20</h2>
+<!-- Coded by Syahrul R. -->
+</center>
+</body>
+</html>
